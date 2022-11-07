11434ee1932b48fc43d4ee4e16a16cf5ec4a78cc
donthephan <donthephan@gmail.com>
2022-11-07 02:17:09 -0500

    all test types complete for header

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -5,44 +5,68 @@
  *    3.  Interact with those elements
  *    4.  Assert that the results are as expected
  *
- *  2. getBy, findBy, queryBy, getAllBuy, findAllBy, queryAllBy.
- *    What's the dif? => https://www.youtube.com/watch?v=Yghw9FkNGsc&list=PL4cUxeGkcC9gm4_-5UsNmLqMosM-dzuvQ&index=5&ab_channel=TheNetNinja
+ *  2. What's the dif? => https://www.youtube.com/watch?v=Yghw9FkNGsc&list=PL4cUxeGkcC9gm4_-5UsNmLqMosM-dzuvQ&index=5&ab_channel=TheNetNinja
+ *    1.  getBy,
+ *    2.  findBy
+ *    3.  queryBy
+ *    4.  getAllBuy, findAllBy, queryAllBy.
  */
 
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-/** GET BY */
+/** 2.1 - GET BY - BASIC */
 
-/** getByText (ideal) */
-it /* can also use 'test' */('should render same text passed into title prop using getByText', () => {
+// getByText (ideal)
+it(/* can also use 'test' */ 'should render same text passed into title prop using getByText', () => {
   render(<Header title='My Header' />);
-  const headingElement = screen.getByText(/my header/i); 
+  const headingElement = screen.getByText(/my header/i);
   expect(headingElement).toBeInTheDocument();
 });
 
-/** getByRole (ideal) */
+// getByRole (ideal)
 it('should render same text passed into title prop using getByRole', () => {
   render(<Header title='My Header' />);
   const headingElement = screen.getByRole('heading', {
     name: 'My Header',
-  }); 
+  });
   expect(headingElement).toBeInTheDocument();
 });
 
-/** getByTitle (LESS ideal) */
+// getByTitle (LESS ideal)
 //* this one checks the h3 w/ the title attr
 it('should render same text passed into title prop using getByTitle', () => {
   render(<Header title='My Header' />);
-  const headingElement = screen.getByTitle('Header'); 
+  const headingElement = screen.getByTitle('Header');
   expect(headingElement).toBeInTheDocument();
 });
 
-/** getByTitle (LEAST ideal) */
+// getByTitle (LEAST ideal)
 it('should render same text passed into title prop using getByTestId', () => {
   render(<Header title='My Header' />);
-  const headingElement = screen.getByTestId('header-1'); 
+  const headingElement = screen.getByTestId('header-1');
   expect(headingElement).toBeInTheDocument();
 });
 
-/** FIND BY */
+/** 2.2 - FIND BY - ASYNC */
+
+it('should render same text passed into title prop using findByText', async () => {
+  render(<Header title='My Header' />);
+  const headingElement = await screen.findByText(/my header/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+/** 2.3 - QUERY BY - ALLOWS NO MATCH (NULL) w/o error */
+
+it(/* can also use 'test' */ 'should render same text passed into title prop using queryByText', () => {
+  render(<Header title='My Header' />);
+  const headingElement = screen.queryByText(/dogs/i); // We don't expect a match
+  expect(headingElement).not.toBeInTheDocument();
+});
+
+/** 2.4 - ALL */
+it('should render same text passed into title prop using getAllByRole', () => {
+  render(<Header title='My Header' />);
+  const headingElements = screen.getAllByRole('heading');
+  expect(headingElements.length).toBe(2);
+});
